aa1feb5dd1ad89c9ee4edd0c1922f59f6f8cbed5
Sneha-2804 <sagupta28.04@gmail.com>
2025-11-09 21:32:36 +0530

    Commit

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner : "678e52542a1e4e3a18721f2c",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
